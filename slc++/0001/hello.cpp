2d7f09e99d73a3ea1bfd90e954cd3f686465303c
sooof <sooof@me.com>
2020-06-17 21:00:54 -0700

    sooof learn c++

---
+++ slc++/0001/hello.cpp
@@ -0,0 +1,7 @@
+// #include "./github.com/BjarneStroustrup/Programming-_Principles_and_Practice_Using_Cpp/std_lib_facilities.h"
+#include "../lib/std_lib_facilities.h"
+int main()
+{
+	cout << "Hello, World!\n";
+	return 0;
+}
